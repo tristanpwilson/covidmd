a4cf9a3ac3cfcb344de0043187f1629081665e16
Tristan <tristanphillipwilson@gmail.com>
2020-04-21 12:53:43 -0400

    Data 4-21-20

--- js/latest.js
+++ js/latest.js
@@ -3,38 +3,38 @@
 //Function to be called in main js
 function latestData () {
   
-countiesData.features[24].properties.history = [39,57,85,108,150,195,245,290,349,423,581,790,1066,1244,1415,1662,1985,2331,2758,3125,3609,4045,4371,5529,6185,6968,7694,8228,8936,9472,10032,10734,11572,12308,12830,13684,];	
-countiesData.features[24].properties.historydeaths = [0,0,1,1,2,3,3,3,4,4,4,5,10,11,15,18,24,29,42,54,68,92,104,124,139,172,207,236,262,302,351,372,408,463,486,516,];
-countiesData.features[24].properties.historyrecoveries = [3,3,3,3,3,3,3,3,3,3,3,25,32,39,43,53,69,81,159,159,159,184,288,365,365,365,431,456,603,607,607,736,736,771,914,917,];
-countiesData.features[24].properties.dates = ["3/16/2020", "3/17/2020", "3/18/2020", "3/19/2020", "3/20/2020", "3/21/2020", "3/22/2020", "3/23/2020", "3/24/2020", "3/25/2020", "3/26/2020", "3/27/2020", "3/28/2020", "3/29/2020", "3/30/2020", "3/31/2020", "4/1/2020", "4/2/2020", "4/3/2020", "4/4/2020", "4/5/2020", "4/6/2020", "4/7/2020", "4/8/2020","4/9/2020","4/10/2020","4/11/2020","4/12/2020","4/13/2020","4/14/2020","4/15/2020","4/16/2020","4/17/2020","4/18/2020","4/19/2020","4/20/2020"];
+countiesData.features[24].properties.history = [39,57,85,108,150,195,245,290,349,423,581,790,1066,1244,1415,1662,1985,2331,2758,3125,3609,4045,4371,5529,6185,6968,7694,8228,8936,9472,10032,10734,11572,12308,12830,13684,14193,];
+countiesData.features[24].properties.historydeaths = [0,0,1,1,2,3,3,3,4,4,4,5,10,11,15,18,24,29,42,54,68,92,104,124,139,172,207,236,262,302,351,372,408,463,486,516,584,];
+countiesData.features[24].properties.historyrecoveries = [3,3,3,3,3,3,3,3,3,3,3,25,32,39,43,53,69,81,159,159,159,184,288,365,365,365,431,456,603,607,607,736,736,771,914,917,930,];
+countiesData.features[24].properties.dates = ["3/16/2020", "3/17/2020", "3/18/2020", "3/19/2020", "3/20/2020", "3/21/2020", "3/22/2020", "3/23/2020", "3/24/2020", "3/25/2020", "3/26/2020", "3/27/2020", "3/28/2020", "3/29/2020", "3/30/2020", "3/31/2020", "4/1/2020", "4/2/2020", "4/3/2020", "4/4/2020", "4/5/2020", "4/6/2020", "4/7/2020", "4/8/2020","4/9/2020","4/10/2020","4/11/2020","4/12/2020","4/13/2020","4/14/2020","4/15/2020","4/16/2020","4/17/2020","4/18/2020","4/19/2020","4/20/2020","4/21/2020"];
 
 
 //cases
-countiesData.features[0].properties.cases = 32;
-countiesData.features[1].properties.cases = 1098;
-countiesData.features[2].properties.cases = 1875;
-countiesData.features[3].properties.cases = 1511;
-countiesData.features[4].properties.cases = 114;
-countiesData.features[5].properties.cases = 39;
-countiesData.features[6].properties.cases = 335;
+countiesData.features[0].properties.cases = 56;
+countiesData.features[1].properties.cases = 1157;
+countiesData.features[2].properties.cases = 1542;
+countiesData.features[3].properties.cases = 1925;
+countiesData.features[4].properties.cases = 116;
+countiesData.features[5].properties.cases = 41;
+countiesData.features[6].properties.cases = 330;
 countiesData.features[7].properties.cases = 134;
-countiesData.features[8].properties.cases = 392;
-countiesData.features[9].properties.cases = 23;
-countiesData.features[10].properties.cases = 616;
+countiesData.features[8].properties.cases = 401;
+countiesData.features[9].properties.cases = 25;
+countiesData.features[10].properties.cases = 632;
 countiesData.features[11].properties.cases = 4;
-countiesData.features[12].properties.cases = 226;
-countiesData.features[13].properties.cases = 538;
-countiesData.features[14].properties.cases = 28;
-countiesData.features[15].properties.cases = 2647;
-countiesData.features[16].properties.cases = 3583;
-countiesData.features[17].properties.cases = 26;
+countiesData.features[12].properties.cases = 232;
+countiesData.features[13].properties.cases = 565;
+countiesData.features[14].properties.cases = 29;
+countiesData.features[15].properties.cases = 2768;
+countiesData.features[16].properties.cases = 3734;
+countiesData.features[17].properties.cases = 27;
 countiesData.features[18].properties.cases = 107;
 countiesData.features[19].properties.cases = 11;
 countiesData.features[20].properties.cases = 22;
-countiesData.features[21].properties.cases = 123;
-countiesData.features[22].properties.cases = 166;
-countiesData.features[23].properties.cases = 34;
-countiesData.features[24].properties.statecases = 13684;
+countiesData.features[21].properties.cases = 126;
+countiesData.features[22].properties.cases = 176;
+countiesData.features[23].properties.cases = 33;
+countiesData.features[24].properties.statecases = 14193;
 
 //prevcases
 countiesData.features[0].properties.prevcases = 33;
@@ -61,53 +61,53 @@ countiesData.features[20].properties.prevcases = 19;
 countiesData.features[21].properties.prevcases = 117;
 countiesData.features[22].properties.prevcases = 145;
 countiesData.features[23].properties.prevcases = 33;
-countiesData.features[24].properties.prevstatecases = 12830;
+countiesData.features[24].properties.prevstatecases = 13684;
 
 //deaths
-countiesData.features[0].properties.deaths = 1;
-countiesData.features[1].properties.deaths = 48;
-countiesData.features[2].properties.deaths = 50;
-countiesData.features[3].properties.deaths = 54;
-countiesData.features[4].properties.deaths = 3;
+countiesData.features[0].properties.deaths = 2;
+countiesData.features[1].properties.deaths = 52;
+countiesData.features[2].properties.deaths = 59;
+countiesData.features[3].properties.deaths = 58;
+countiesData.features[4].properties.deaths = 4;
 countiesData.features[5].properties.deaths = 0;
 countiesData.features[6].properties.deaths = 31;
 countiesData.features[7].properties.deaths = 3;
-countiesData.features[8].properties.deaths = 20;
+countiesData.features[8].properties.deaths = 23;
 countiesData.features[9].properties.deaths = 1;
-countiesData.features[10].properties.deaths = 30;
+countiesData.features[10].properties.deaths = 33;
 countiesData.features[11].properties.deaths = 0;
-countiesData.features[12].properties.deaths = 6;
+countiesData.features[12].properties.deaths = 7;
 countiesData.features[13].properties.deaths = 13;
 countiesData.features[14].properties.deaths = 1;
-countiesData.features[15].properties.deaths = 106;
-countiesData.features[16].properties.deaths = 112;
+countiesData.features[15].properties.deaths = 116;
+countiesData.features[16].properties.deaths = 118;
 countiesData.features[17].properties.deaths = 2;
-countiesData.features[18].properties.deaths = 2;
+countiesData.features[18].properties.deaths = 3;
 countiesData.features[19].properties.deaths = 0;
 countiesData.features[20].properties.deaths = 1;
 countiesData.features[21].properties.deaths = 3;
-countiesData.features[22].properties.deaths = 3;
+countiesData.features[22].properties.deaths = 2;
 countiesData.features[23].properties.deaths = 0;
-countiesData.features[24].properties.deaths = 516;
+countiesData.features[24].properties.deaths = 584;
 
 //prevdeaths
 countiesData.features[0].properties.prevdeaths = 1;
-countiesData.features[1].properties.prevdeaths = 40;
-countiesData.features[2].properties.prevdeaths = 44;
-countiesData.features[3].properties.prevdeaths = 46;
+countiesData.features[1].properties.prevdeaths = 48;
+countiesData.features[2].properties.prevdeaths = 50;
+countiesData.features[3].properties.prevdeaths = 54;
 countiesData.features[4].properties.prevdeaths = 3;
 countiesData.features[5].properties.prevdeaths = 0;
-countiesData.features[6].properties.prevdeaths = 27;
+countiesData.features[6].properties.prevdeaths = 31;
 countiesData.features[7].properties.prevdeaths = 3;
-countiesData.features[8].properties.prevdeaths = 17;
+countiesData.features[8].properties.prevdeaths = 20;
 countiesData.features[9].properties.prevdeaths = 1;
-countiesData.features[10].properties.prevdeaths = 25;
+countiesData.features[10].properties.prevdeaths = 30;
 countiesData.features[11].properties.prevdeaths = 0;
-countiesData.features[12].properties.prevdeaths = 1;
-countiesData.features[13].properties.prevdeaths = 11;
+countiesData.features[12].properties.prevdeaths = 6;
+countiesData.features[13].properties.prevdeaths = 13;
 countiesData.features[14].properties.prevdeaths = 1;
-countiesData.features[15].properties.prevdeaths = 80;
-countiesData.features[16].properties.prevdeaths = 98;
+countiesData.features[15].properties.prevdeaths = 106;
+countiesData.features[16].properties.prevdeaths = 112;
 countiesData.features[17].properties.prevdeaths = 2;
 countiesData.features[18].properties.prevdeaths = 2;
 countiesData.features[19].properties.prevdeaths = 0;
@@ -115,7 +115,7 @@ countiesData.features[20].properties.prevdeaths = 1;
 countiesData.features[21].properties.prevdeaths = 3;
 countiesData.features[22].properties.prevdeaths = 3;
 countiesData.features[23].properties.prevdeaths = 0;
-countiesData.features[24].properties.prevdeaths = 425;
+countiesData.features[24].properties.prevdeaths = 516;
 
 //recoveries
 countiesData.features[0].properties.recoveries = 0;
@@ -128,7 +128,7 @@ countiesData.features[6].properties.recoveries = 12;
 countiesData.features[7].properties.recoveries = 0;
 countiesData.features[8].properties.recoveries = 0;
 countiesData.features[9].properties.recoveries = 0;
-countiesData.features[10].properties.recoveries = 20;
+countiesData.features[10].properties.recoveries = 152;
 countiesData.features[11].properties.recoveries = 0;
 countiesData.features[12].properties.recoveries = 0;
 countiesData.features[13].properties.recoveries = 36;
@@ -142,9 +142,9 @@ countiesData.features[20].properties.recoveries = 0;
 countiesData.features[21].properties.recoveries = 0;
 countiesData.features[22].properties.recoveries = 0;
 countiesData.features[23].properties.recoveries = 0;
-countiesData.features[24].properties.recoveries = 917;
+countiesData.features[24].properties.recoveries = 930;
 
 //prevrecoveries
-countiesData.features[24].properties.prevrecoveries = 914;
+countiesData.features[24].properties.prevrecoveries = 917;
 
 }  
